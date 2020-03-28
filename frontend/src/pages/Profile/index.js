86cbc26e32e9e13a556840f3dbb4d8b654691bcb
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-03-28 00:21:21 -0300

    create incidents

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
@@ -7,16 +7,18 @@ import api from '../../services/api';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([])
+    
+    const history = useHistory();
+    
     const ongName = localStorage.getItem('ongName');
     const ongID = localStorage.getItem('ongID')
-    
+
     useEffect(() => {
         api.get('profile', {
             headers: {
                 Authorization: ongID,
             }
         }).then(response => {
-            console.log(response.data, ongID)
             setIncidents(response.data)
         })
     }, []);
@@ -34,6 +36,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -41,7 +48,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={()=>{handleLogout()}} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
